a54dc6d4fb6be2bf682ce0cf1a3d104cda33ab08
Kerollos Magdy <kerolloz@yahoo.com>
2022-08-20 19:27:40 +0000

    add IsVerifable & validations to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,38 @@
 import { getModelForClass, prop } from '@typegoose/typegoose';
+import Joi from 'joi';
 import BaseModel from './BaseModel';
 import {
   CanGenerateToken,
   HasPassword,
   ICanGenerateToken,
   IHasPassword,
+  IIsVerifiable,
+  IsVerifiable,
 } from './mixins';
 
+@IsVerifiable
 @HasPassword
 @CanGenerateToken
 export class User extends BaseModel {
-  @prop({ required: true, unique: true })
+  @prop({ required: true, unique: true, trim: true })
   email!: string;
+
+  @prop({ required: true, trim: true })
+  name!: string;
 }
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export interface User extends IHasPassword, ICanGenerateToken {}
+export interface User extends IHasPassword, ICanGenerateToken, IIsVerifiable {}
 
 export const UserModel = getModelForClass(User, {
   options: { customName: 'users' },
 });
+
+export const userLoginValidations = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const userSignupValidations = {
+  name: Joi.string().required(),
+  ...userLoginValidations,
+};
