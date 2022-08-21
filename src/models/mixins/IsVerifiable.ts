9fc57f13aa43d461f6c24a12b25fa262c612006c
Kerollos Magdy <kerolloz@yahoo.com>
2022-08-21 13:21:34 +0000

    hide isVerified from user json

--- src/models/mixins/IsVerifiable.ts
+++ src/models/mixins/IsVerifiable.ts
@@ -3,11 +3,13 @@ import { Constructor } from '../../types';
 
 export interface IIsVerifiable {
   isVerified: boolean;
+  _isVerified: boolean;
 }
 
 export function IsVerifiable<TBase extends Constructor>(Base: TBase) {
   abstract class _IsVerifiable extends Base implements IIsVerifiable {
-    @prop({ default: false })
+    @prop({ default: false, alias: 'isVerified' })
+    _isVerified!: boolean;
     isVerified!: boolean;
   }
 
