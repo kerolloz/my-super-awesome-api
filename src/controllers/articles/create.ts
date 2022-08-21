0a4b0608d15641cd1241c2cdec260ac9fcc66d1e
Kerollos Magdy <kerolloz@yahoo.com>
2022-08-21 15:18:43 +0000

    add image to article

--- src/controllers/articles/create.ts
+++ src/controllers/articles/create.ts
@@ -1,11 +1,80 @@
+import formidable, { File } from 'formidable';
 import { IAuthRequest } from '../../auth';
-import { endpoint } from '../../core';
-import { articleValidations, ArticleModel } from '../../models';
+import { BAD_REQUEST, endpoint, HttpException } from '../../core';
+import { ArticleModel } from '../../models';
+import { ImageUploader } from '../../services';
 
-export default endpoint({ body: articleValidations }, async (req) => ({
-  status: 201,
-  content: await ArticleModel.create({
-    ...req.body,
-    user: (req as IAuthRequest).currentUser,
-  }),
-}));
+export default endpoint(async (req) => {
+  const aacceptedContentType = 'multipart/form-data';
+  const isAcceptedContentType =
+    req.headers['content-type'] === aacceptedContentType;
+
+  if (isAcceptedContentType) {
+    throw new HttpException(BAD_REQUEST, { message: 'Invalid content type' });
+  }
+  const MAX_FILE_SIZE = 10 * 1024 * 1024; // meagbytes to bytes
+  const form = formidable({
+    maxFileSize: MAX_FILE_SIZE,
+    keepExtensions: true,
+    allowEmptyFiles: false,
+  });
+  const body = await new Promise<{
+    title: string;
+    content: string;
+    file?: File;
+  }>((resolve, reject) =>
+    form.parse(req, (formError, fields, files) => {
+      if (formError) {
+        console.error(formError);
+        return reject(
+          new HttpException(BAD_REQUEST, { message: 'Invalid file' }),
+        );
+      }
+      const { title, content } = fields;
+      if (!title || !content) {
+        return reject(
+          new HttpException(BAD_REQUEST, {
+            message: "Add 'title' and 'content' fields",
+          }),
+        );
+      }
+      if (typeof title !== 'string' || typeof content !== 'string') {
+        return reject(
+          new HttpException(BAD_REQUEST, {
+            message: "'title' and 'content' fields must be strings",
+          }),
+        );
+      }
+
+      const image = files.image as File;
+      if (image && !image?.mimetype?.startsWith('image/')) {
+        return reject(
+          new HttpException(BAD_REQUEST, {
+            message: 'file must be an image',
+          }),
+        );
+      }
+
+      return resolve({
+        title,
+        content,
+        file: image,
+      });
+    }),
+  );
+
+  const { title, content, file } = body;
+  let image: string | undefined = undefined;
+  if (file) {
+    image = await ImageUploader.upload(file.filepath);
+  }
+  return {
+    status: 201,
+    content: await ArticleModel.create({
+      title,
+      content,
+      image,
+      user: (req as IAuthRequest).currentUser,
+    }),
+  };
+});
--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,22 +1,20 @@
 import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
-import Joi from 'joi';
+import { URL_REGEX } from '../regex/url';
 import PaginatedModel from './PaginatedModel';
 import { User, UserModel } from './User';
 
 export class Article extends PaginatedModel {
-  @prop({ required: true })
+  @prop({ required: true, trim: true, minlength: 3 })
   title!: string;
 
-  @prop({ required: true })
+  @prop({ required: true, trim: true, minlength: 3 })
   content!: string;
 
   @prop({ required: true, ref: () => UserModel.modelName })
   user!: Ref<User>;
+
+  @prop({ match: URL_REGEX })
+  image!: string;
 }
 
 export const ArticleModel = getModelForClass(Article);
-
-export const articleValidations = {
-  title: Joi.string().required(),
-  content: Joi.string().required(),
-};
