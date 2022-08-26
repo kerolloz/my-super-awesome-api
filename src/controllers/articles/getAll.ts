911156fed5bf11ee69df502745ff69de174958b4
Kerollos Magdy <kerolloz@yahoo.com>
2022-08-26 15:00:56 +0200

    return articles sorted by newly created first

--- src/controllers/articles/getAll.ts
+++ src/controllers/articles/getAll.ts
@@ -2,5 +2,5 @@ import { endpoint } from '../../core';
 import { ArticleModel } from '../../models';
 
 export default endpoint(async () => ({
-  content: await ArticleModel.find(),
+  content: await ArticleModel.find().sort({ createdAt: -1 }),
 }));
