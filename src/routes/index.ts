368fba5e5d5f1149035a746bf46064038b4852a6
Kerollos Magdy <kerolloz@yahoo.com>
2024-06-20 04:20:01 +0300

    refactor: Remove console.log statements and improve route registration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,9 @@ import fs from 'node:fs/promises';
 import type { FastifyZodInstance } from '../lib/index.js';
 
 export const registerAllRoutes = async (app: FastifyZodInstance) => {
-  console.log('Reading files in the current directory...');
   const files = await fs.readdir(import.meta.dirname);
-  console.log(files);
-
   // Read *.route.ts files in the current directory
   // register them by calling their default export and passing the app instance
-  console.log('Registering routes...');
   await Promise.all(
     files
       .filter((file) => file.endsWith('.route.js'))
@@ -18,5 +14,4 @@ export const registerAllRoutes = async (app: FastifyZodInstance) => {
         console.log(`Route ${file} registered`);
       }),
   );
-  return;
 };
